e3259354cb9519dc4640857ab4b66bfce8fbcdd8
DaryPet <d7akkord@gmail.com>
2024-02-22 22:42:46 +0100

    HW_11-loading

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { getImages } from './js/pixabay-api';
+import { displayImages } from './js/render-functions';
+import { displayErrorMessage } from './js/render-functions';
+
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#search-input');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const searchText = input.value.trim();
+
+  if (searchText === '') {
+    displayErrorMessage('Please fill imput');
+    return;
+  }
+
+  getImages(searchText)
+    .then(data => {
+      if (data.hits.length === 0) {
+        displayErrorMessage(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+        return;
+      }
+      displayImages(data.hits, gallery);
+      input.value = '';
+    })
+    .catch(error => {
+      console.error('Error during search:', error);
+      displayErrorMessage('Error');
+    });
+});
